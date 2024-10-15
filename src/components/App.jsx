87d5777ad9110d133a4c3f440deb99a5e43c7414
jipsa-7 <jipsa.varkey@yahoo.com>
2024-10-14 22:38:59 -0500

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Entry from "./Entry";
+import emojipedia from "../emojipedia";
+
+// function createEntry(emojiTerm) {
+//   return (
+{
+  /* <Entry
+  key={emojiTerm.id}
+  emoji={emojiTerm.emoji}
+  name={emojiTerm.name}
+  description={emojiTerm.meaning}
+/>; */
+}
+//   );
+// }
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">
+        {emojipedia.map((emojipedia) => (
+          <Entry
+            key={emojipedia.id}
+            emoji={emojipedia.emoji}
+            name={emojipedia.name}
+            description={emojipedia.meaning}
+          />
+        ))}
+      </dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./../public/styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+var numbers = [3, 56, 2, 48, 5];
+
+/////Map -Create a new array by doing something with each item in an array.
+const newNumber1 = numbers.map((x) => x * 2);
+console.log(newNumber1);
+
+//////Filter - Create a new array by keeping the items that return true.
+const newNumber2 = numbers.filter((num) => num < 10);
+console.log(newNumber2);
+
+////Reduce - Accumulate a value by doing something to each item in an array.
+var newNumber3 = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log(newNumber3);
+
+////Find - find the first item that matches from an array.
+const newNumber4 = numbers.find((num) => num > 10);
+console.log(newNumber4);
+
+////FindIndex - find the index of the first item that matches.
+const newNumber5 = numbers.findIndex((num) => num > 10);
+console.log(newNumber5);
